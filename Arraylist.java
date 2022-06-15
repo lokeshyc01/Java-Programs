49a70344db93f14f29bfbe1381568834419438bc
lokeshyc01 <100062082+lokeshyc01@users.noreply.github.com>
2022-06-15 18:11:34 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist {
+	// Main driver method
+	public static void main(String[] args)
+	{
+		
+		List<Integer> al = new ArrayList<>();
+
+		// Adding elements to our ArrayList
+		// using add() method
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(1);
+		al.add(2);
+
+		System.out.println(al);
+
+		al.remove(1);
+
+		
+		al.remove(1);
+
+		System.out.println(al);
+	}
+}
